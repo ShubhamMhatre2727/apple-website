49e21de06181905e7e8b04c760f3516179b68a76
ShubhamMhatre2727 <72384869+ShubhamMhatre2727@users.noreply.github.com>
2024-07-27 06:29:23 +0000

    closure section updated

--- src/componetns/ClosureLook.jsx
+++ src/componetns/ClosureLook.jsx
@@ -1,6 +1,6 @@
 import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 
 const ClosureLook = () => {
     const list = [{
@@ -31,25 +31,38 @@ const ClosureLook = () => {
     },[])
 
     const [id, setId] = useState(1);
+    const ref = useRef(null);
+
+    function handleClick(i){
+        const children = gsap.utils.toArray(ref.current.children);
+        children.forEach((child, idx)=>{
+                if (idx === i)
+                    child.style.border = "4px solid #fff";
+                else
+                    child.style.border = "none";
+            })
+        setId(i);
+    }
+    
   return (
-    <div>
-        <h2 className='text-2xl font-semibold'>Take a Closure Look</h2>
-        <div className='w-full flex flex-col items-center'>
+    <div className=" h-[80vh] flex flex-col items-center justify-between">
+        <h2 className='w-full  text-start text-2xl font-semibold'>Take a Closure Look</h2>
             <img id='image' src={list[id].url} alt="" />
+        <div className="mx-auto text-center">
 
-            <h3 className='font-bold'>{list[id].text}</h3>
+            <h3 className='font-bold w-72'>{list[id].text}</h3>
 
-            <div className='flex gap-2 items-center'>
-                   <li className='size-6 rounded-full' style={{backgroundImage: "url(public/images/closer-look/all-icon.png)",backgroundPosition: "center",backgroundSize:"cover"
+            <div ref={ref} className='flex gap-2 items-center justify-center py-4'>
+                   <li onClick={()=>handleClick(0)} className='size-6 rounded-full' style={{backgroundImage: "url(public/images/closer-look/all-icon.png)",backgroundPosition: "center",backgroundSize:"cover"
                    }}>
                    </li>
                 {
-                    ["#8f8a81","#202630","#c9c8c3","#242526"].map((color)=>(
-                        <li className=' size-6 rounded-full' style={{background:color}} key={color}></li>
+                    ["#8f8a81","#202630","#c9c8c3","#242526"].map((color,idx)=>(
+                        <li onClick={()=>handleClick(idx+1)} className=' size-6 rounded-full' style={{background:color}} key={color}></li>
                     ))
                 }
             </div>
-        </div>
+    </div>
     </div>
   )
 }
